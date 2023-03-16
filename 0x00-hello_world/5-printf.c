493cd755b2abc23a63153a5c5131bc68d535822c
Besbol100 <basel.youssef47@gmail.com>
2023-03-16 16:36:51 -0700

    PrintF

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print using Printf function
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
