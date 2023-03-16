00f5d08cf3ddd7d9e4dbb1664470ff7b66bfebad
Besbol100 <basel.youssef47@gmail.com>
2023-03-16 16:29:43 -0700

    I feel kinda dizy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+	/*
+         * main - Entry point
+         * Description: It's a C program that prints using PUT Function
+         * Return: Always 0 (Success)
+         *
+        */
 int main(void)
 {
-	/*
-	 * main - start running point
-	 * Description: It's a C program that prints using PUT Function
-	 * Return: is 0 for Success return
-	 *
-	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
