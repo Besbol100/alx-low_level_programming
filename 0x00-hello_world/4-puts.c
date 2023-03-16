94fabc13a58646c875c58f7a374faaa6c610df54
Besbol100 <basel.youssef47@gmail.com>
2023-03-16 16:20:18 -0700

    NWAD

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 int main(void)
 {
+	/*
+	 * main - The Entry Point
+	 * Description: It's a C program that prints using PUT Function
+	 * Return: is 0 for Success return
+	 *
+	 */
 	puts("Programming is like building a multilingual puzzle");
+	puts("\n");
 	return (0);
 }
