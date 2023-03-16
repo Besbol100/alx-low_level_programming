a20dfdbd430f49c60d8fe618c74125fe40dce258
Besbol100 <basel.youssef47@gmail.com>
2023-03-16 16:42:14 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: UNIX is a simple operating system
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n");
-	return (1);
+	return (0);
 }
